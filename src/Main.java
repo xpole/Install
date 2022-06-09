6c7c05686c49ec42ddff3e950b47ce31791fc013
xpole <newyeardreamer@gmail.com>
2022-06-09 13:47:16 +0300

    Union cycle

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,17 @@ import java.util.List;
 
 public class Main {
 
-    public static void createDir(String path, String dirname, StringBuilder logStr) {
-        File dir = new File(path + "/" + dirname);
+    public static void createDir(String path, StringBuilder logStr) {
+        File dir = new File(path);
         if (dir.mkdir())
             logStr.append("Каталог " + dir + " создан" + "\n");
     }
 
-    public static void createFile(String path, String filename, StringBuilder logStr) {
-        File file = new File(path, filename);
+    public static void createFile(String path, StringBuilder logStr) {
+        File file = new File(path);
         try {
             if (file.createNewFile())
-                logStr.append("В директории " + path + " создан файл " + file.getName() + "\n");
+                logStr.append("Файл " + file + " создан" + "\n");
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
@@ -24,25 +24,26 @@ public class Main {
 
     public static void main(String[] args) {
         StringBuilder string = new StringBuilder();
-        File dir = new File("D://Games");
-        String dirTest = "D://";
-        createDir(dirTest, "Games2", string);
+        String[] dirNames = {"D://Games2/src",
+                "D://Games2/res",
+                "D://Games2/savegames",
+                "D://Games2/temp",
+                "D://Games2/src/main",
+                "D://Games2/src/test",
+                "D://Games2/res/drawables",
+                "D://Games2/res/vectors",
+                "D://Games2/res/icons"};
+        String[] fileNames = {"D://Games2/src/main/Main.java",
+                "D://Games2/src/main/Utils.java",
+                "D://Games2/temp/temp.txt"};
 
-        List<String> dirs = Arrays.asList("src", "res", "savegames", "temp");
-        for (String s : dirs) {
-            createDir("D://Games2", s, string);
+        for (String s : dirNames) {
+            createDir(s, string);
         }
-        createDir("D://Games2/src", "main", string);
-        createDir("D://Games2/src", "test", string);
 
-        List<String> dirsRes = Arrays.asList("drawables", "vectors", "icons");
-        for (String d : dirsRes
-        ) {
-            createDir("D://Games2/res", d, string);
+        for (String f : fileNames) {
+            createFile(f, string);
         }
-        createFile("D://Games2/src/main", "Main.java", string);
-        createFile("D://Games2/src/main", "Utils.java", string);
-        createFile("D://Games2/temp", "temp.txt", string);
 
         try (FileWriter writer = new FileWriter(new File("D://Games2/temp", "temp.txt"))) {
             // запись в файл
